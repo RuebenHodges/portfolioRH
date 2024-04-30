f2b482c101fa30c9d741789dd981ba2aaf89a0b9
RuebenHodges <hodgesr@umich.edu>
2024-04-30 18:45:36 -0400

    navbar prog

---
+++ portfolio/src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+function Navbar() {
+  return (
+        <nav>
+            <ul>
+                <li><a href="/">Skills</a></li>
+                <li><a href="/about">Experience</a></li>
+                <li><a href="/services">Projects</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
+        </nav>
+  );
+}
+
+export default Navbar;
