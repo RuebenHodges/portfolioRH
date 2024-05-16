1e33945a3201204f0e49b5f77f16600581095e02
RuebenHodges <hodgesr@umich.edu>
2024-05-16 13:18:35 -0400

    nav functionality TBD

---
+++ portfolio/src/HireMe.js
@@ -0,0 +1,20 @@
+import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import reactLogo from './assets/react.png';
+//personal pictures
+import pfp from './assets/pfp.png';
+import pfp2 from './assets/pfp2.png';
+//skills sections
+import htmlCss from './assets/htmlCss.png';
+import javaLogo from './assets/Java-logo.png';
+import jsLogo from './assets/JavaScript-logo.png';
+import sqlLogo from './assets/sql.png';
+
+function HireMe() {
+  return (
+    <Navbar/>
+  );
+}
+
+export default HireMe;
