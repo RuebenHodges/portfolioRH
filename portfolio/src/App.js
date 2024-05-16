7f039c56b9486282ff4c2d2736bd8d06ea38e84f
RuebenHodges <hodgesr@umich.edu>
2024-05-16 12:51:12 -0400

    home page almost done sizing next

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import reactLogo from './assets/react.png';
 //personal pictures
 import pfp from './assets/pfp.png';
+import pfp2 from './assets/pfp2.png';
 //skills sections
 import htmlCss from './assets/htmlCss.png';
 import javaLogo from './assets/Java-logo.png';
@@ -35,17 +36,23 @@ function App() {
           </div>
 
           <div className="aboutMe">
-            <h1>Software Engineer Specializing in Innovative Web Development</h1>
-            <p> 
-            Greetings!Im a recent graduate holding a Bachelor’s Degree in Software Engineering. 
-            I am passionate about creating user-centric solutions in web development. 
-            Through my academic studies and project work, 
-            I have gained a strong understanding of technologies such as Java, SQL, 
-            and modern web frameworks, which I use to build seamless and scalable websites. 
-            I am a team player who thrives on collaboration and welcomes challenges that help me to enhance my skills and contribute to the success of my team. 
-            Currently located in the Detroit Metropolitan Area, 
-            I am actively seeking opportunities where 
-            I can innovate, grow, and make a meaningful impact.</p>
+          <h1>Software Engineer Specializing in Innovative Web Development</h1>
+            <div className='aboutMeContainer'>
+              
+              <img className='pfp2' src={pfp2}></img>
+                <div className='textContainer'>
+                  <p> 
+                  Greetings! Im a recent graduate holding a Bachelor’s Degree in Software Engineering. 
+                  I am passionate about creating user-centric solutions in web development. 
+                  Through my academic studies and project work, 
+                  I have gained a strong understanding of technologies such as Java, SQL, 
+                  and modern web frameworks, which I use to build seamless and scalable websites. 
+                  I am a team player who thrives on collaboration and welcomes challenges that help me to enhance my skills and contribute to the success of my team. 
+                  Currently located in the Detroit Metropolitan Area, 
+                  I am actively seeking opportunities where 
+                  I can innovate, grow, and make a meaningful impact.</p>
+                </div>
+            </div>
           </div>
 
           <div className="introduction">
