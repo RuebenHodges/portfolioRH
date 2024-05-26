c5dc3f577e49e030422c40e8b5b0b772e816ac30
RuebenHodges <hodgesr@umich.edu>
2024-05-25 20:33:33 -0400

    updated contact

--- portfolio/src/HireMe.js
+++ portfolio/src/HireMe.js
@@ -1,20 +1,30 @@
 import './App.css';
+import './HireMe.css';
 //import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import React from 'react';
-import Navbar from './components/Navbar';
-import reactLogo from './assets/react.png';
-//personal pictures
-import pfp from './assets/pfp.png';
-import pfp2 from './assets/pfp2.png';
-//skills sections
-import htmlCss from './assets/htmlCss.png';
-import javaLogo from './assets/Java-logo.png';
-import jsLogo from './assets/JavaScript-logo.png';
-import sqlLogo from './assets/sql.png';
+//assets
+import resume from './assets/resume.pdf';
+import emailIcon from './assets/email-icon-122.png';
 
 function HireMe() {
   return (
-    <p>contact</p>
+    <div className="App">
+      <body>
+        <main>
+          <div className="contactMe">
+          <h1>Want to get in Touch?</h1>
+          <img className='email' src={emailIcon}/>
+
+          <h3>best way to contact me is 
+            <a className='socials' href="mailto:hodgesr@umich.edu"> Email </a>  or  
+            <a className='socials' href='https://www.linkedin.com/in/ruebenhodges/'> linkedin </a></h3>
+          <a href={resume} download={"RuebensResume"}>
+            <button className="resumeButton" type='button'> Download Resume</button>
+          </a>
+          </div>
+        </main>
+      </body>
+    </div>
   );
 }
 
